16624e94ebd561cca33d003c84bc565996352cc4
okgPL <okgPL@yahoo.pl>
2017-03-03 23:40:27 +0100

    3.03.17: Generator logs
    -Added generator logs
    -Fixed a bug when ranged/melee only items were checked from SR list on
    each map

--- UBravery.cpp
+++ UBravery.cpp
@@ -186,6 +186,7 @@ void UBravery::play(int map)
 		case 1: cout << "Map: The Twisted Treeline" << endl << endl; break;
 		case 2: cout << "Map: Howling Abyss" << endl << endl; break;
 		}
+		clog << "Map: " << map << endl;
 		ChampionType champtype;
 		//////////Champion//////////
 		int xadj = rand() % iAdjectives; //Randomizes adjective to champion 
@@ -208,26 +209,59 @@ void UBravery::play(int map)
 			sSelectedChamp = sChampionsMelee[number];
 			champtype = MELEE;
 		}		
+		clog << "Champion: " << sSelectedChamp;
 
 		//////////First ability//////////
 		if (sSelectedChamp == "Udyr") number = rand() % 4;	//Udyr exception - he's able to max R
 		else number = rand() % 3;
 		switch (number)
 		{
-		case 0: cout << "Max: Q" << endl; break;
-		case 1: cout << "Max: W" << endl; break;
-		case 2: cout << "Max: E" << endl; break;
-		case 3: cout << "Max: R" << endl; break;
+		case 0: 
+		{
+			cout << "Max: Q" << endl; 
+			clog << "Max: Q" << endl;	break;
+		}
+		case 1:
+		{
+			cout << "Max: W" << endl;
+			clog << "Max: W" << endl;	break;
+		}
+		case 2:
+		{
+			cout << "Max: E" << endl;
+			clog << "Max: E" << endl;	break;
+		}
+		case 4:
+		{
+			cout << "Max: R" << endl;
+			clog << "Max: R" << endl;	break;
+		}
 		}
 		if (sSelectedChamp == "Kha'Zix")				//Kha'Zix can upgrade his abilities
 		{
 			number = rand() % 4;
 			switch (number)
 			{
-			case 0: cout << "First upgrade: Q" << endl; break;
-			case 1: cout << "First upgrade: W" << endl; break;
-			case 2: cout << "First upgrade: E" << endl; break;
-			case 3: cout << "First upgrade: R" << endl; break;
+			case 0:
+			{
+				cout << "First upgrade: Q" << endl; 
+				clog << "First upgrade: Q" << endl;	break; 
+			}
+			case 1:
+			{
+				cout << "First upgrade: W" << endl;
+				clog << "First upgrade: W" << endl;	break;
+			}
+			case 2:
+			{
+				cout << "First upgrade: E" << endl;
+				clog << "First upgrade: R" << endl;	break;
+			}
+			case 3:
+			{
+				cout << "First upgrade: R" << endl;
+				clog << "First upgrade: R" << endl;	break;
+			}
 			}
 		}
 		if (sSelectedChamp == "Gangplank")				//Gangplank can upgrade his R
@@ -235,9 +269,22 @@ void UBravery::play(int map)
 			number = rand() % 3;
 			switch (number)
 			{
-			case 0: cout << "First upgrade: Death's Daughter" << endl; break;
-			case 1: cout << "First upgrade: Raise Morale" << endl; break;
-			case 2: cout << "First upgrade: Fire at Will" << endl; break;
+			case 0: 
+			{ 
+				cout << "First upgrade: Death's Daughter" << endl; 
+				clog << "First upgrade: Death's Daughter" << endl;
+				break; 
+			}
+			case 1: 
+			{
+				cout << "First upgrade: Raise Morale" << endl;
+				clog << "First upgrade: Raise Morale" << endl;  break;
+			}
+			case 2: 
+			{ 
+				cout << "First upgrade: Fire at Will" << endl; 
+				clog << "First upgrade: Fire at Will" << endl; break; 
+			}
 			}
 		}
 		//////////Spells//////////
@@ -248,16 +295,26 @@ void UBravery::play(int map)
 			{
 				number -= 7;
 				cout << sSpellsARAM[number];
+				clog<<"Spell 1: "<< sSpellsARAM[number] << endl;
 			}
-			else cout << sSpells[number];
+			else
+			{
+				cout << sSpells[number];
+				clog<<"Spell 1: " << sSpells[number] << endl;
+			}
 		else //SR and TT
 		{
 			if (number >= 7)
 			{
 				number -= 7;
 				cout << sSpellsNORMAL[number];
+				clog<<"Spell 1:" << sSpellsNORMAL[number] << endl;
 			}
-			else cout << sSpells[number];
+			else
+			{
+				cout << sSpells[number];
+				clog << "Spell 1: " << sSpells[number] << endl;
+			}
 		}
 		//Second spell
 		while (true)
@@ -269,8 +326,16 @@ void UBravery::play(int map)
 					if (number == number2) continue;		//Cannot set two the same spells
 					else
 					{
-						if(map == 2) cout << ", " << sSpellsARAM[number2] << endl;
-						else cout << ", " << sSpellsNORMAL[number2] << endl;
+						if (map == 2)
+						{
+							cout << ", " << sSpellsARAM[number2] << endl;
+							clog << "Spell 2: " << sSpellsARAM[number2] << endl;
+						}
+						else
+						{
+							cout << ", " << sSpellsNORMAL[number2] << endl;
+							clog << "Spell 2:" << sSpellsNORMAL[number2] << endl;
+						}
 						break;
 					}
 				}
@@ -284,6 +349,7 @@ void UBravery::play(int map)
 		//////////Keystone//////////
 		number = rand() % 9;
 		cout << "Keystone: " << sMasteries[number] << endl;
+		clog << "Keystone: " << sMasteries[number] << endl;
 
 		/////////Items//////////
 		int NumOfItems = 5;
@@ -291,6 +357,7 @@ void UBravery::play(int map)
 		if (sSelectedChamp == "Viktor")				//Viktor - Hex core instead of boots
 		{
 			cout << "1. " << "Perfect Hex Core";
+			clog << "Item 1: Perfect Hex Core" << endl;
 			switch (rand() % 4)		//He can upgrade abilities with that item
 			{
 			case 0: cout << " (first upgrade: Q)" << endl; break;
@@ -303,7 +370,8 @@ void UBravery::play(int map)
 		else					// boots
 		{
 			int number = rand() % iBoots;
-			cout << "1. " << sBoots[number] << endl;;
+			cout << "1. " << sBoots[number] << endl;
+			clog << "Item 1: " << sBoots[number] << endl;
 		}
 		int items[6];
 		bool isSupportItem = false;
@@ -348,8 +416,16 @@ void UBravery::play(int map)
 							continue;
 						}
 						else isSupportItem = true;
-					if (sSelectedChamp == "Cassiopeia") cout << i + 1 << ". " << sItemsSR[items[i]] << endl;
-					else cout << i + 2 << ". " << sItemsSR[items[i]] << endl;
+						if (sSelectedChamp == "Cassiopeia")
+						{
+							cout << i + 1 << ". " << sItemsSR[items[i]] << endl;
+							clog << "Item " << i + 1 << ": " << sItemsSR[items[i]] << endl;
+						}
+						else
+						{
+							cout << i + 2 << ". " << sItemsSR[items[i]] << endl;
+							clog << "Item " << i + 2 << ": " << sItemsSR[items[i]] << endl;
+						}
 					break;
 				}
 				case 1:
@@ -365,14 +441,22 @@ void UBravery::play(int map)
 						}
 						else isSupportItem = true;
 
-						if ((champtype == MELEE && sItemsSR[items[i]] == "Runaan's Hurricane")
-							|| (champtype == RANGED && (sItemsSR[items[i]] == "Ravenous Hydra" || sItemsSR[items[i]] == "Titanic Hydra")))
+						if ((champtype == MELEE && sItemsTT[items[i]] == "Runaan's Hurricane")
+							|| (champtype == RANGED && (sItemsTT[items[i]] == "Ravenous Hydra" || sItemsTT[items[i]] == "Titanic Hydra")))
 						{
 							--i;
 							continue;
 						}
-					if (sSelectedChamp == "Cassiopeia") cout << i + 1 << ". " << sItemsTT[items[i]] << endl;
-					else cout << i + 2 << ". " << sItemsTT[items[i]] << endl;
+						if (sSelectedChamp == "Cassiopeia")
+						{
+							cout << i + 1 << ". " << sItemsTT[items[i]] << endl;
+							clog << "Item " << i + 1 << ": " << sItemsTT[items[i]] << endl;
+						}
+						else
+						{
+							cout << i + 2 << ". " << sItemsTT[items[i]] << endl;
+							clog << "Item " << i + 2 << ": " << sItemsTT[items[i]] << endl;
+						}
 					break;
 				}
 				case 2:
@@ -388,14 +472,22 @@ void UBravery::play(int map)
 						}
 						else isSupportItem = true; 
 
-						if ((champtype == MELEE && sItemsSR[items[i]] == "Runaan's Hurricane")
-							|| (champtype == RANGED && (sItemsSR[items[i]] == "Ravenous Hydra" || sItemsSR[items[i]] == "Titanic Hydra")))
+						if ((champtype == MELEE && sItemsARAM[items[i]] == "Runaan's Hurricane")
+							|| (champtype == RANGED && (sItemsARAM[items[i]] == "Ravenous Hydra" || sItemsARAM[items[i]] == "Titanic Hydra")))
 						{
 							--i;
 							continue;
 						}
-					if (sSelectedChamp == "Cassiopeia") cout << i + 1 << ". " << sItemsARAM[items[i]] << endl;
-					else cout << i + 2 << ". " << sItemsARAM[items[i]] << endl;
+						if (sSelectedChamp == "Cassiopeia")
+						{
+							cout << i + 1 << ". " << sItemsARAM[items[i]] << endl;
+							clog <<"Item "<< i + 1 << ": " << sItemsARAM[items[i]] << endl;
+						}
+						else
+						{
+							cout << i + 2 << ". " << sItemsARAM[items[i]] << endl;
+							clog << "Item " << i + 2 << ": " << sItemsARAM[items[i]] << endl;
+						}
 					break;
 				}
 				}
